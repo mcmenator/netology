0f15676f8a3e22858be08f1e588f173616b0b7b1
mcmenator <mcm@bk.ru>
2024-01-02 19:26:39 +0300

    222

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ def solve(phrases: list):
         if text == text[::-1]: # сравните фразу с ней же, развернутой наоборот (через [::-1])
            result.append(phrase)
     return result
+
+def solve(cook_book: list, person: int):
+    result = []
+    # самостоятельно напишите код решения:
+    for i in cook_book:
+        res_i=(f'{i[0]}: ')
+        for j in i[1]:
+            res_i+=(f'{j[0]} {j[1]*person} {j[2]}, ')
+        result.append(res_i[0:-2])
+    return result
